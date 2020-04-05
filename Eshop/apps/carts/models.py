5b27d52c6c3490167b2034222f8a406d73e52660
oshevelo <alex.shevelo@gmail.com>
2020-04-05 11:42:24 +0300

    Revert "cart-models"

--- Eshop/apps/carts/models.py
+++ Eshop/apps/carts/models.py
@@ -1,11 +1,3 @@
 from django.db import models
-from django.contrib.auth.models import User
-#from ... import Product
 
-class Cart(model.Model):
-    customer = model.ForeignKey(User, blank=True)
-    products = models.ManyToManyField(Product, blank=True)
-    total = models.FloatField(default=0.00, max_digits=100)
-    
-class Discount(Cart, discount):
-    Cart.total = Cart.total - Cart.total * discount
+# Create your models here.
