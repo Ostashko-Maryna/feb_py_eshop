439acb9a6a3a3679fda31c2677d5208e944c9b4e
oshevelo <alex.shevelo@gmail.com>
2020-04-09 21:21:50 +0300

    Update models.py

--- Eshop/apps/user_profiles/models.py
+++ Eshop/apps/user_profiles/models.py
@@ -7,8 +7,8 @@ class UserProfile(models.Model):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
     date_of_birth = models.DateField(blank=True, null=True)
-    photo = models.ImageField(upload_to='users/%Y/%m/%d', blank=True)
-    delivery_address = models.CharField(max_length=200)
+    photo = models.ImageField(upload_to='users/%Y/%m/%d', blank=True)#FIXME: move to static/users/%Y/%m/%d
+    delivery_address = models.CharField(max_length=200)#FIXME: SEPARATE model
     vip_status = models.BooleanField(default=False)
 
     def __str__(self):
