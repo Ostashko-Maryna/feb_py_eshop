7557442f260084c249ab36af4d661ebcfc87b965
VitaliiBarlit <mia2vitalii@gmail.com>
2020-04-16 19:05:59 +0300

    Carts: models, views

--- Eshop/apps/carts/models.py
+++ Eshop/apps/carts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime
 
-# Create your models here.
+
+
+class Cart(models.Model):
+    active = models.BooleanField(default=True) # cart not empty
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.DateTimeField(default=datetime.now)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+        
+        
+class CartItem(models.Model):
+    product = models.ForeignKey('products.Product', on_delete=models.SET_NULL, null=True, blank=True)
+    quantity = models.IntegerField(default=1)       
+    price = models.FloatField(blank=True)
+    cart = models.ForeignKey('Cart', on_delete=models.CASCADE)
