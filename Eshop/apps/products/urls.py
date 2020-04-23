3d0d1a427e0e835a21670a79b7fe0b4c5b39f763
dapodkovko <dapodkovko@gmail.com>
2020-04-23 15:44:22 +0300

    drf views

--- Eshop/apps/products/urls.py
+++ Eshop/apps/products/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:product_id>/', views.detail, name='detail'),
+    # path('', views.index, name='index'),
+    path('', views.ProductList.as_view(), name='list'),
+    path('<int:product_id>/', views.ProductDetail.as_view(), name='detail'),
+    path('<int:product_id>/reviews/', views.ProductReviews.as_view(), name='list_product_reviews'),
+    path('reviews/', views.ReviewList.as_view(), name='list'),
+    path('reviews/<int:review_id>/', views.ReviewDetail.as_view(), name='detail'),
 ]
