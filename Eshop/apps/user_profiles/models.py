e943f80c7236d565d1f546aa6bf390a1de166107
Slavik Sobolenko <slava.z.boguslava@gmail.com>
2020-04-30 04:03:56 +0300

    created list view + detail view

--- Eshop/apps/user_profiles/models.py
+++ Eshop/apps/user_profiles/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 #from localflavor import ua
 
 
 class UserProfile(models.Model):
-    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
     date_of_birth = models.DateField(blank=True, null=True)
@@ -13,6 +16,17 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.user_profile.save()
 
 
 class DeliveryAddress(models.Model):
--- Eshop/apps/user_profiles/views.py
+++ Eshop/apps/user_profiles/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import Http404
+from .models import UserProfile, DeliveryAddress
 
-# Create your views here.
+from rest_framework import generics
+from .serializers import UserProfileSerializer, DeliveryAddressSerializer
+from django.shortcuts import get_object_or_404
+from .models import UserProfile, DeliveryAddress
+
+
+class UserProfileList(generics.ListCreateAPIView):
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer
+
+
+class UserProfileDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = UserProfileSerializer
+
+    def get_object(self):
+        obj = get_object_or_404(UserProfile, pk=self.kwargs.get('user_profile_id'))
+        return obj
+
+
+class DeliveryAddressList(generics.ListCreateAPIView):
+    queryset = DeliveryAddress.objects.all()
+    serializer_class = DeliveryAddressSerializer
+
+
+class DeliveryAddressDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = DeliveryAddressSerializer
+
+    def get_object(self):
+        obj = get_object_or_404(DeliveryAddress, pk=self.kwargs.get('delivery_address_id'))
+        return obj
+
+"""
+def index(request):
+    user_profile_list = UserProfile.objects.all()
+    return HttpResponse("Hello, OTHER world. You're at the polls index.")
+
+"""
+
+
