bc21f536d0e6d29ff910abe06b66d755a2512dd3
oshevelo <alex.shevelo@gmail.com>
2020-05-03 12:46:12 +0300

    Update serializers.py

--- Eshop/apps/galleries/serializers.py
+++ Eshop/apps/galleries/serializers.py
@@ -4,8 +4,10 @@ from apps.galleries.models import Gallery
 
 
 class GallerySerializer(serializers.ModelSerializer):
-
+    #image = serializers.ImageField(write_only=True)
+    #url = serializers.CharField(source='logo_url', read_only=True)
+    
     class Meta:
         model = Gallery
-        fields = ['id', 'product', 'name', 'image', 'size', 'size_x', 'size_y']
+        fields = ['id', 'product', 'name', 'image', 'size', 'size_x', 'size_y'] #url
 
