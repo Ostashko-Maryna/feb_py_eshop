8f1afd5891d7bd1831717ada3eeffe1b5688ef4b
Denys Kotsiuba <denis.kotsiuba@gmail.com>
2020-05-17 10:00:36 +0300

    add post test

--- Eshop/apps/orders/tests.py
+++ Eshop/apps/orders/tests.py
@@ -32,7 +32,22 @@ class OrdersTestAPI(APITestCase):
                 'order_cost': '0.00',
             }]
         })
-
+    '''
+    def test_post_to_order_list(self):
+        response = self.c.post('/orders/user1/')
+        self.assertEqual(response.status_code, 201)
+        print(response.json())
+        self.assertEqual(response.json(), {
+            'id': response.json()[id],
+            'order_number': self.order1.order_number,
+            'order_date': self.order1.order_date.strftime('%Y-%m-%dT%H:%M:%S.%fZ'),
+            'order_status': self.order1.order_status,
+            'order_payment': self.order1.order_payment,
+            'order_shipment': self.order1.order_shipment,
+            'user': self.order1.user.id,
+            'order_cost': '0.00',
+        })
+    '''
     def test_orderitem_list(self):
         response = self.c.get('/orders/3/orderitems/?limit=1')
         self.assertEqual(response.status_code, 200)
@@ -46,4 +61,15 @@ class OrdersTestAPI(APITestCase):
                 'product': self.orderitem1.product.id,
                 'amount_of_products': self.orderitem1.amount_of_products
             }]
+        })
+
+    def test_post_to_orderitem_list(self):
+        response = self.c.post('/orders/3/orderitems/')
+        print(response.json())
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.json(), {
+            'id': 7,
+            'order': None,
+            'product': None,
+            'amount_of_products': 1
         })
