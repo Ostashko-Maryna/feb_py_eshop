ac8f96da63394cbc641949dc3f469afa952f4799
Denys Kotsiuba <denis.kotsiuba@gmail.com>
2020-05-20 23:02:55 +0300

    order tests

--- Eshop/apps/orders/tests.py
+++ Eshop/apps/orders/tests.py
@@ -3,78 +3,254 @@ from rest_framework.test import APIClient, APITestCase
 from apps.orders.models import Order, OrderItem
 from django.contrib.auth.models import User
 from apps.products.models import Product
+from apps.user_profiles.models import UserProfile
+import uuid
+from django.db.models.deletion import ProtectedError
 
 class OrdersTestAPI(APITestCase):
     def setUp(self):
         self.user = User.objects.create_user('user_name', 'user_name@gmail.com', 'password')
-        self.order1 = Order.objects.create(user=self.user)
-        self.order2 = Order.objects.create(user=self.user)
+        #self.userprofile = UserProfile.objects.create(user=self.user, first_name='Denys', last_name='Kotsiuba', vip_status=True)
         self.product = Product.objects.create(name='test product', created_by=self.user)
-        self.orderitem1 = self.order1.orderitem.create(product = self.product)
-        self.orderitem2 = self.order1.orderitem.create(product=self.product)
+        self.order = Order.objects.create(user=self.user)
+        self.orderitem = self.order.orderitem.create(order=self.order, product=self.product)
         self.c = APIClient()
+        self.maxDiff = None
 
-    def test_order_list(self):
+    def test_1(self):
+        print(self.user.id)
+        #print(self.userprofile.vip_status)
+        print(self.order.id)
+        print(self.orderitem.id)
         response = self.c.get('/orders/user1/?limit=1')
         self.assertEqual(response.status_code, 200)
+        print(response.json())
         self.assertEqual(response.json(), {
-            'count': 2,
-            'next': 'http://testserver/orders/user1/?limit=1&offset=1',
+            'count': 1,
+            'next': None,
             'previous': None,
             'results': [{
-                'id': self.order1.id,
-                'order_number': self.order1.order_number,
-                'order_date': self.order1.order_date.strftime('%Y-%m-%dT%H:%M:%S.%fZ'),
-                'order_status': self.order1.order_status,
-                'order_payment': self.order1.order_payment,
-                'order_shipment': self.order1.order_shipment,
-                'user': self.order1.user.id,
+                'id': self.order.id,
+                'order_number': self.order.order_number,
+                'order_date': self.order.order_date.strftime('%Y-%m-%dT%H:%M:%S.%fZ'),
+                'order_status': self.order.order_status,
+                'order_payment': self.order.order_payment,
+                'order_shipment': self.order.order_shipment,
+                'user': self.order.user.id,
                 'order_cost': '0.00',
             }]
         })
 
-    def test_post_to_order_list(self):
-        
-        response = self.c.post('/orders/user1/')
-        print((response.json().get('id')))
+    def test_2(self):
+        print(self.user.id)
+        print(self.order.id)
+        print(self.orderitem.id)
+        response = self.c.get('/orders/2/orderitems/?limit=1')
         print(response.json())
-        print('__'*30)
-        self.assertEqual(response.status_code, 201)
-        created_order = Order.objects.get(id=response.json().get('id'))
-        print(created_order)
-        #FIXME: add validation against obj form db
-        self.assertEqual(response.json(), {
-            'id': created_order.id,
-            'order_number': created_order.order_number,
-            'order_date': response.json().get('order_date'),
-            'order_status': response.json().get('order_status'),
-            'order_payment': response.json().get('order_payment'),
-            'order_shipment': response.json().get('order_shipment'),
-            'user': response.json().get('user'),
-            'order_cost': response.json().get('order_cost'),
-        })
-
-    def test_orderitem_list(self):
-        response = self.c.get('/orders/3/orderitems/?limit=1')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json(), {
-            'count': 2,
-            'next': 'http://testserver/orders/3/orderitems/?limit=1&offset=1',
+            'count': 1,
+            'next': None,
             'previous': None,
             'results': [{
-                'id': self.orderitem1.id,
-                'order': self.orderitem1.order.id,
-                'product': self.orderitem1.product.id,
-                'amount_of_products': self.orderitem1.amount_of_products
+                'id': self.orderitem.id,
+                'order': self.orderitem.order.id,
+                'product': self.orderitem.product.id,
+                'amount_of_products': self.orderitem.amount_of_products
             }]
         })
 
-    def test_post_to_orderitem_list(self):
-        response = self.c.post('/orders/3/orderitems/')
-        self.assertEqual(response.status_code, 201)
+    def test_3(self):
+        print(self.user.id)
+        print(self.order.id)
+        print(self.orderitem.id)
+        response = self.c.get('/orders/3/')
+        print(response.json())
+        self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json(), {
-            'id': response.json().get('id'),
-            'order': response.json().get('order'),
-            'product': response.json().get('product'),
-            'amount_of_products': response.json().get('amount_of_products')
+            'id': self.order.id,
+            'order_number': self.order.order_number,
+            'order_date': self.order.order_date.strftime('%Y-%m-%dT%H:%M:%S.%fZ'),
+            'order_status': self.order.order_status,
+            'order_payment': self.order.order_payment,
+            'order_shipment': self.order.order_shipment,
+            'user': self.order.user.id,
+            'order_cost': '0.00',
         })
+
+    def test_4(self):
+        print(self.user.id)
+        print(self.order.id)
+        print(self.orderitem.id)
+        response = self.c.get('/orders/orderitem4/')
+        print(response.json())
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {
+            'id': self.orderitem.id,
+            'order': self.orderitem.order.id,
+            'product': self.orderitem.product.id,
+            'amount_of_products': self.orderitem.amount_of_products
+        })
+
+    def test_5(self):
+        print(self.user.id)
+        print(self.order.id)
+        print(self.orderitem.id)
+        response = self.c.post(
+            '/orders/user5/',data={
+                'user': self.user.id
+            }
+        )
+        print(response.json())
+        self.assertEqual(response.status_code, 201)
+        created_order = Order.objects.get(id=response.json()['id'])
+        print(created_order.id)
+        self.assertEqual(response.json(), {
+            'id': created_order.id,
+            'order_number': created_order.order_number,
+            'order_date': created_order.order_date.strftime('%Y-%m-%dT%H:%M:%S.%fZ'),
+            'order_status': created_order.order_status,
+            'order_payment': created_order.order_payment,
+            'order_shipment': created_order.order_shipment,
+            'user': created_order.user.id,
+            'order_cost': str(created_order.order_cost),
+        })
+
+    def test_6(self):
+        print(self.user.id)
+        print(self.order.id)
+        print(self.orderitem.id)
+        response = self.c.post(
+            '/orders/3/orderitems/', data={
+                'order': self.order.id,
+                'product': self.product.id
+            }
+        )
+        print(response.json())
+        self.assertEqual(response.status_code, 201)
+        created_orderitem = OrderItem.objects.get(id=response.json()['id'])
+        self.assertEqual(response.json(), {
+            'id': created_orderitem.id,
+            'order': created_orderitem.order.id,
+            'product': created_orderitem.product.id,
+            'amount_of_products': created_orderitem.amount_of_products
+        })
+
+    def test_7(self):
+        print(self.user.id)
+        print(self.order.id)
+        print(self.orderitem.id)
+        print(self.order.order_number)
+        response = self.c.put(
+            '/orders/8/',
+            data={
+                'id': self.order.id,
+                'order_number': str(uuid.uuid4())[:23],
+                'order_date': self.order.order_date.strftime('%Y-%m-%dT%H:%M:%S.%fZ'),
+                'order_status': self.order.order_status,
+                'order_payment': Order.Payment.portmone,
+                'order_shipment': self.order.order_shipment,
+                'user': self.order.user.id,
+                'order_cost': '0.00',
+            }
+        )
+        print(response.json())
+        self.assertEqual(response.status_code, 200)
+        created_order = Order.objects.get(id=response.json()['id'])
+        self.assertEqual(response.json(), {
+            'id': created_order.id,
+            'order_number': created_order.order_number,
+            'order_date': created_order.order_date.strftime('%Y-%m-%dT%H:%M:%S.%fZ'),
+            'order_status': created_order.order_status,
+            'order_payment': created_order.order_payment,
+            'order_shipment': created_order.order_shipment,
+            'user': created_order.user.id,
+            'order_cost': str(created_order.order_cost),
+        })
+
+    def test_8(self):
+        print(self.user.id)
+        print(self.order.id)
+        print(self.orderitem.id)
+        response = self.c.put(
+            '/orders/orderitem9/',
+            data={
+                'id': self.orderitem.id,
+                'order': self.orderitem.order.id,
+                'product': self.orderitem.product.id,
+                'amount_of_products': 2
+            }
+        )
+        print(response.json())
+        self.assertEqual(response.status_code, 200)
+        created_orderitem = OrderItem.objects.get(id=response.json()['id'])
+        self.assertEqual(response.json(), {
+            'id': created_orderitem.id,
+            'order': created_orderitem.order.id,
+            'product': created_orderitem.product.id,
+            'amount_of_products': created_orderitem.amount_of_products
+        })
+
+    def test_9(self):
+        print(self.user.id)
+        print(self.order.id)
+        print(self.orderitem.id)
+        print(self.order.order_number)
+        response = self.c.patch(
+            '/orders/10/',
+            data={
+                'order_number': str(uuid.uuid4())[:23],
+                'order_payment': Order.Payment.portmone,
+            }
+        )
+        print(response.json())
+        self.assertEqual(response.status_code, 200)
+        created_order = Order.objects.get(id=response.json()['id'])
+        self.assertEqual(response.json(), {
+            'id': created_order.id,
+            'order_number': created_order.order_number,
+            'order_date': created_order.order_date.strftime('%Y-%m-%dT%H:%M:%S.%fZ'),
+            'order_status': created_order.order_status,
+            'order_payment': created_order.order_payment,
+            'order_shipment': created_order.order_shipment,
+            'user': created_order.user.id,
+            'order_cost': str(created_order.order_cost),
+        })
+
+    def test_90(self):
+        print(self.user.id)
+        print(self.order.id)
+        print(self.orderitem.id)
+        response = self.c.patch(
+            '/orders/orderitem11/',
+            data={
+                'amount_of_products': 20
+            }
+        )
+        print(response.json())
+        print(response)
+        self.assertEqual(response.status_code, 200)
+        created_orderitem = OrderItem.objects.get(id=response.json()['id'])
+        self.assertEqual(response.json(), {
+            'id': created_orderitem.id,
+            'order': created_orderitem.order.id,
+            'product': created_orderitem.product.id,
+            'amount_of_products': created_orderitem.amount_of_products
+        })
+
+    def test_91(self):
+        print(self.user.id)
+        print(self.order.id)
+        print(self.orderitem.id)
+        with self.assertRaises(ProtectedError):
+            response = self.c.delete('/orders/12/')
+
+    def test_92(self):
+        print(self.user.id)
+        print(self.order.id)
+        print(self.orderitem.id)
+        response = self.c.delete('/orders/orderitem13/')
+        print(response.status_code)
+        print(response)
+        self.assertEqual(response.status_code, 204)
+
