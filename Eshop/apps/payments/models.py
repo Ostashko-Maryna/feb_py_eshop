1f3902659109d4d1e05ad3e6770c35bc708038d6
maksym.valyn <mvalyn@gmail.com>
2020-05-21 22:07:17 +0300

    migrations bugs fixed

--- Eshop/apps/payments/models.py
+++ Eshop/apps/payments/models.py
@@ -3,58 +3,55 @@ from django.contrib.postgres.fields import JSONField
 from django_fsm import FSMField, transition
 
 paymentsystem_list = [
-        ('portmone', 'portmone'),
-        ('cash', 'cash'),
+    ('portmone', 'portmone'),
+    ('cash', 'cash'),
+]
+
+class Status:
+    submitted = 'submitted'
+    processing = 'processing'
+    completed = 'completed'
+    suspended = 'suspended'     
+    declined = 'declined'
+
+status_list = [
+    (Status.submitted, 'submitted'),
+    (Status.processing, 'processing'),
+    (Status.completed, 'completed'),
+    (Status.suspended, 'suspended'),
+    (Status.declined, 'declined'),
 ]
 
 
 class Payments(models.Model):
-    user = models.ForeignKey('auth.User', on_delete=models.PROTECT)
-    order = models.ForeignKey('orders.Order', on_delete=models.PROTECT)
+    user = models.ForeignKey('auth.User', on_delete=models.PROTECT, 
+        null=True, blank=False
+    )
+    order = models.ForeignKey('orders.Order', on_delete=models.PROTECT,
+        null=True, blank=False
+    )
     paymentsystem = models.CharField(max_length=10, 
         choices=paymentsystem_list, default='portmone'
     )
-    billAmount = models.FloatField()
+    billAmount = models.FloatField(default=0.0)
     payment_date = models.DateTimeField(auto_now_add=True)
+    status = FSMField(default=status_list[0], choices=status_list)
     
-    '''
-     /order/<order_id>/pay
-     class PayOrder(generic.CreateView):
-        def create(self)
-            order = get_object_or_404(Order, pk=self.kwargs.order_id)
-            paymentsystem = self.request.json['paymentsystem']
-            order.paymentsystem = paymentsystem
-            payment = Payment.create_payment(
-                user=self.request.user, 
-                order=order,
-                paymentsystem=paymentsystem
-            )
-                
-    '''
-    @classmethod
-    def create_payment(cls, user, order, paymentsystem):
-        new_payment = cls(user)
-        new_payment.order = order
-        new_payment.paymentsystem = paymentsystem
-        new_payment.billAmount = order.order_cost
-        new_payment.save()
-        return new_payment
-
     
-    class Status:
-        submitted = 'submitted'
-        processing = 'processing'
-        completed = 'completed'
-        suspended = 'suspended'		
-        declined = 'declined'
+    # ~ class Status:
+        # ~ submitted = 'submitted'
+        # ~ processing = 'processing'
+        # ~ completed = 'completed'
+        # ~ suspended = 'suspended'     
+        # ~ declined = 'declined'
     
-    status_list = [
-        (Status.submitted, 'submitted'),
-        (Status.processing, 'processing'),
-        (Status.completed, 'completed'),
-        (Status.suspended, 'suspended'),
-        (Status.declined, 'declined'),
-    ]
+    # ~ status_list = [
+        # ~ (Status.submitted, 'submitted'),
+        # ~ (Status.processing, 'processing'),
+        # ~ (Status.completed, 'completed'),
+        # ~ (Status.suspended, 'suspended'),
+        # ~ (Status.declined, 'declined'),
+    # ~ ]
 
     '''
     status = models.CharField(max_length=10, 
@@ -62,9 +59,7 @@ class Payments(models.Model):
         default=Status.submitted,
     )
     '''
-
-    status = FSMField(default=status_list[0], choices=status_list)
-
+    
     @transition(field=status, source=['submitted', 'suspended'],
         target='processing'
     )
@@ -85,6 +80,15 @@ class Payments(models.Model):
     def decline(self):
         pass
 
+    @classmethod
+    def create_payment(cls, user, order):
+        new_payment = cls(user)
+        new_payment.order = order
+        new_payment.paymentsystem = paymentsystem
+        new_payment.billAmount = order.order_cost
+        new_payment.save()
+        return new_payment
+
 
     class PaymentSystem:
         portmone = 'portmone'
@@ -97,11 +101,11 @@ class Payments(models.Model):
 
     class Meta:
         verbose_name_plural = 'Payments'
-    '''
+    '''     
     @classmethod
     def create_payment(cls, user, paymentsystem, sum, order)
         pass
-    '''
+    ''' 
     def __str__(self):
         return 'user {} has payed for order {}'.format(self.user, self.order)
 
@@ -109,18 +113,18 @@ class Payments(models.Model):
 class PaymentSystemLog(models.Model):
     #data sent to payments_system
     shopOrderNumber = models.ForeignKey('orders.Order', 
-        on_delete=models.PROTECT
+        on_delete=models.PROTECT, null=True, blank=False
     )
-    payeeId = models.IntegerField()
+    payeeId = models.IntegerField(null=True, blank=False)
     dt = models.DateTimeField(auto_now=True) #sent_at
-    billAmount = models.FloatField()
+    billAmount = models.FloatField(null=True, blank=False)
     raw_data = JSONField()
 
     #response from payments_system
-    SHOPBILLID = models.IntegerField()
-    SHOPORDERNUMBER = models.CharField(max_length=50)
-    BILL_AMOUNT = models.FloatField()
-    RESULT = models.SmallIntegerField()
+    SHOPBILLID = models.IntegerField(null=True, blank=False)
+    SHOPORDERNUMBER = models.CharField(max_length=50, null=True, blank=False)
+    BILL_AMOUNT = models.FloatField(null=True, blank=False)
+    RESULT = models.SmallIntegerField(null=True, blank=False)
     raw_response = JSONField()
 
     def __str__(self):
--- Eshop/apps/payments/tests.py
+++ Eshop/apps/payments/tests.py
@@ -1,3 +1,63 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from rest_framework.test import APIClient
+from .models import Payments
+from apps.orders.models import Order
+from datetime import date
 
-# Create your tests here.
+
+class PaymentsTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create()
+        self.order = Order.objects.create()
+        self.payment = Payments.objects.create(
+            user = self.user,
+            order = self.order,
+            paymentsystem = 'portmone',
+            billAmount = 100.0,
+            payment_date = date.today(),
+            status = 'completed',
+        )
+        self.client = APIClient()
+    
+    def test_payments_get(self):
+        print()
+        response = self.client.get('/payments/')
+        self.assertEqual(response.status_code, 200)
+        print(response.json())
+        self.assertEqual(response.json(),[{
+            'id': self.payment.id, 
+            'payment_date': str(
+                self.payment.payment_date.isoformat().replace('+00:00', 'Z')
+            ), 
+            'user': self.user.id, 
+            'order': self.order.id, 
+            'paymentsystem': str(self.payment.paymentsystem)
+        }])
+    
+    def test_payments_post(self):
+        request = self.client.post('/payments/',{
+            'id': self.payment.id, 
+            'payment_date': date.today(), 
+            'user': self.user.id, 
+            'order': self.order.id, 
+            'paymentsystem': self.payment.paymentsystem
+        })
+        print(request)
+        self.assertEqual(request.status_code, 201)
+
+    def test_payments_detail(self):
+        print()
+        number =  self.payment.id
+        response = self.client.get('/payments/{}/'.format(number))
+        self.assertEqual(response.status_code, 200)
+        print(response.json())
+        self.assertEqual(response.json(),{
+            'id': self.payment.id, 
+            'payment_date': str(
+                self.payment.payment_date.isoformat().replace('+00:00', 'Z')
+            ), 
+            'user': self.user.id, 
+            'order': self.order.id, 
+            'paymentsystem': str(self.payment.paymentsystem)
+        })
