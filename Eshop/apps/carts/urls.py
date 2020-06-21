6429a5989c192234c64d5d74762619dd6704d72a
VitaliiBarlit <49618974+VitaliiBarlit@users.noreply.github.com>
2020-06-21 10:55:39 +0300

    Add files via upload

--- Eshop/apps/carts/urls.py
+++ Eshop/apps/carts/urls.py
@@ -3,9 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:user_id>/cartlist/', views.CartList.as_view(), name='CartList'),
-    path('<int:user_id>/<int:cart_id>/', views.CartDetail.as_view(), name='CartDetail'),
-    path('<int:user_id>/create/', views.CartItemCreate.as_view(), name='CartItemCreate'),
-    path('<int:user_id>/<int:cart_id>/cilist/', views.CartItemList.as_view(), name='CartItemList'),
-    path('<int:user_id>/<int:cart_id>/<int:cartitem_id>/', views.CartItemDetail.as_view(), name='CartItemDetail'),
+    path('', views.CartList.as_view(), name='CartList'),
+    path('<int:cart_id>/', views.CartDetail.as_view(), name='CartDetail'),
+    path('<int:cart_id>/cartitem/', views.CartItemList.as_view(), name='CartItemList'),
+    path('<int:cart_id>/cartitem/<int:cartitem_id>/', views.CartItemDetail.as_view(), name='CartItemDetail'),
 ]
