37453176c82db875c07ed5285b3266f0e7d3bea3
Alex shevelo <oshevelo@softserveinc.com>
2020-06-21 12:05:34 +0300

    fix tests

--- Eshop/apps/carts/tests.py
+++ Eshop/apps/carts/tests.py
@@ -3,6 +3,9 @@ from apps.carts.models import Cart, CartItem
 from django.contrib.auth.models import User
 from apps.products.models import Product
 
+def login_user(client, user, password='cartnamepwrd'):
+    client.login(username=user.username, password=password)
+
 class CartTestAPI(APITestCase):
     def setUp(self):
         self.user = User.objects.create_user('cartname', 'cartname@yyy.yyy', 'cartnamepwrd',)
@@ -26,14 +29,11 @@ class CartTestAPI(APITestCase):
         print(self.user.id)
         print(self.cart.id)
         # print(self.cartitem.id)
+        login_user(self.c, self.user)
         response = self.c.get('/cart/1/')
         self.assertEqual(response.status_code, 200)
         print(response.json())
-        self.assertEqual(response.json(), {
-            'count': 1,
-            'next': None,
-            'previous': None,
-            'results': [{
+        test_data = {
                 'id': self.cart.id, 
                 'user': self.cart.user.id, 
                 'not_empty': self.cart.not_empty, 
@@ -42,13 +42,34 @@ class CartTestAPI(APITestCase):
                 'total_price': self.cart.total_price, 
                 'cart_list': self.cart.cart_list, 
                 'not_available': self.cart.not_available
-                }]
+        }
+
+        for key, value in response.json().items():
+            print(key)
+            print(test_data[key]==value)
+            if not test_data[key]==value:
+                print(test_data[key])
+                print(value)
+
+        self.assertEqual(response.json(), {
+                'id': self.cart.id, 
+                'user': self.cart.user.id, 
+                'not_empty': self.cart.not_empty, 
+                'created_on': self.cart.created_on.strftime('%Y-%m-%dT%H:%M:%S.%fZ'),
+                'updated_on': self.cart.updated_on.strftime('%Y-%m-%dT%H:%M:%S.%fZ'),
+                'total_price': self.cart.total_price, 
+                'cart_list': self.cart.cart_list, 
+                'not_available': self.cart.not_available
         })
+
     def test_get_cartitem(self):
         print(self.user.id)
         print(self.cart.id)
         print(self.cartitem.id)
+
+        login_user(self.c, self.user)
         response = self.c.get('/cart/1/cartitem/')
+        print(response.json())
         self.assertEqual(response.status_code, 200)
         print(response.json())
         self.assertEqual(response.json(), {
