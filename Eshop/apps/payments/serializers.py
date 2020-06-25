e03584c12be412522fa2072fc78e8c50945f4647
maksym.valyn <mvalyn@gmail.com>
2020-06-25 19:39:32 +0300

    nested serializer

--- Eshop/apps/payments/serializers.py
+++ Eshop/apps/payments/serializers.py
@@ -4,13 +4,13 @@ from .models import Payments
 from apps.orders.models import Order
 
 class OrderInPaymentsSerializer(serializers.ModelSerializer):
-    # id = serializers.IntegerField()
+    id = serializers.IntegerField()
     
 
     class Meta:
         model = Order
         fields = [
-            # 'id',
+            'id',
             'user',
             'order_status',
             'order_payment'
@@ -36,7 +36,7 @@ class PaymentsSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         order_data = validated_data.pop('order')
         created_payment = Payments.objects.create(**validated_data)
-        order = Order.objects.create(**order_data)
+        order = Order.objects.get(id = order_data.get('id'))
         if order:
             created_payment.order = order
             created_payment.save()
@@ -52,7 +52,7 @@ class PaymentsSerializer(serializers.ModelSerializer):
     def update(self, instance, validated_data):
         order_data = validated_data.pop('order')
         updated_payment = super().update(instance, validated_data)
-        order = Order.objects.create(**order_data)
+        order = Order.objects.get(id = order_data.get('id'))
         # order = Order.objects.filter(order_data.get('id'))
         if order:
             updated_payment.order = order
